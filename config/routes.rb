926bbc64a3d29789ff68bbddc5f0a1516237a8f7
satoshitodaka <satoshitodaka0705@gmail.com>
2021-10-01 21:10:44 +0900

    add user model & cotroller, views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   root to: 'questions#index'
 
+  # 自分のユーザーしか操作しないからresourceでOK？
+  resources :users, except: :destroy
+
+  namespace :admin do
+    resources :users, except: [:new, :create]
+  end
+
   resources :questions do
     get 'unsolved', on: :collection
     get 'solved', on: :collection
