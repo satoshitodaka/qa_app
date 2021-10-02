cf1d3eea2a696c4c27516676ee8f0347416802e4
satoshitodaka <satoshitodaka0705@gmail.com>
2021-10-02 12:13:57 +0900

    add admin_views

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,8 @@ class Question < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
 
+  belongs_to :user
+
 
   def close_question
     self.update(solved: true, solved_at: Time.zone.now)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_29_220114) do
+ActiveRecord::Schema.define(version: 2021_10_02_020224) do
 
   create_table "questions", force: :cascade do |t|
     t.string "title", null: false
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2021_09_29_220114) do
     t.datetime "solved_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
