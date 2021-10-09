8ebf9071769fef44883e8250b2ff70ec87ce64cb
satoshitodaka <satoshitodaka0705@gmail.com>
2021-10-09 19:12:54 +0900

    タブの導入

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,9 +5,19 @@ class Question < ApplicationRecord
   belongs_to :user
   has_many :answers
 
+  scope :recent, -> { order(created_at: :desc)}
+
   def close_question
     self.update(solved: true, solved_at: Time.zone.now)
   end
-    private
+  
+  private
+    def self.ransackble_attributes(auth_object = nil)
+      %w[title question_body created_at]
+    end
+
+    def self.ransackble_associations(auth_object = nil)
+      []
+    end
     
 end
