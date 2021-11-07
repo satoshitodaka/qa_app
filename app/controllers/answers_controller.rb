1ae8bc8ea02ed48f84bcb04396eae92f5dd094de
satoshitodaka <satoshitodaka0705@gmail.com>
2021-11-07 23:52:45 +0900

    回答通知メールの配信先修正

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -5,11 +5,15 @@ class AnswersController < BaseController
   # end
 
   def create
-    @answer = current_user.answers.new(answer_params)
+    @answer = current_user.answers.build(answer_params)
 
     if @answer.save
-      @users = User.all.where.not(id: current_user.id)
-      @users.each do |user|
+      # 質問投稿者にメール
+      AnswerMailer.creation_email(@answer, @answer.question.user).deliver_now
+
+      # その他回答者にメール
+      answered_users = User.joins(:answers).where(answers: {question_id: @answer.question.id}).where.not(answers: {user_id: @answer.user.id}).distinct
+      answered_users.each do |user|
         AnswerMailer.creation_email(@answer, user).deliver_now
       end
       redirect_to question_path(@answer.question)
